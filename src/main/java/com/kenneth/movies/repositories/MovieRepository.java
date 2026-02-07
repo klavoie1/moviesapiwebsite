33ad522ceb021fae5613c42befc52758c97c164b
klavoie1 <kenlavoie19@gmail.com>
2026-02-06 22:56:35 -0500

    repository interface for the movie database

--- src/main/java/com/kenneth/movies/repositories/MovieRepository.java
+++ src/main/java/com/kenneth/movies/repositories/MovieRepository.java
@@ -1,4 +1,10 @@
 package com.kenneth.movies.repositories;
 
-public interface MovieRepository {
+import com.kenneth.movies.models.Movie;
+import org.bson.types.ObjectId;
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface MovieRepository extends MongoRepository<Movie, ObjectId> {
 }
