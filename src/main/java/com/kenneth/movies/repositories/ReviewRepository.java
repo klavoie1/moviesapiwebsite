2c2db75d9371bb03a585b72368ad6905f5b3e343
klavoie1 <kenlavoie19@gmail.com>
2026-02-07 10:26:42 -0500

    added repo for reviews

--- src/main/java/com/kenneth/movies/repositories/ReviewRepository.java
+++ src/main/java/com/kenneth/movies/repositories/ReviewRepository.java
@@ -1,4 +1,10 @@
 package com.kenneth.movies.repositories;
 
-public interface ReviewRepository {
+import com.kenneth.movies.models.Review;
+import org.bson.types.ObjectId;
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ReviewRepository extends MongoRepository<Review, ObjectId> {
 }
