8e2b44535d314335429a4d0dc205ccc5dda0962e
klavoie1 <kenlavoie19@gmail.com>
2026-02-06 22:01:42 -0500

    private fields for movie class

--- src/main/java/com/kenneth/movies/Movie.java
+++ src/main/java/com/kenneth/movies/Movie.java
@@ -1,4 +1,34 @@
 package com.kenneth.movies;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.bson.types.ObjectId;
+import org.springframework.data.mongodb.core.mapping.Document;
+import org.springframework.data.annotation.Id;
+
+import java.util.List;
+
+@Document(collection = "movies")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class Movie {
+
+    @Id
+    private ObjectId id;
+
+    private String imbdId;
+
+    private String title;
+
+    private String releaseDate;
+
+    private String trailerLink;
+
+    private String poster;
+
+    private List<String> genres;
+
+    private List<String> backdrops;
 }
