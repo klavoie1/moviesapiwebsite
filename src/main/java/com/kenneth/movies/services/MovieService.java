f58b02f39a63df14efa051d4a72124b170f9c70a
klavoie1 <kenlavoie19@gmail.com>
2026-02-06 22:57:21 -0500

    allMovies method in service class

--- src/main/java/com/kenneth/movies/services/MovieService.java
+++ src/main/java/com/kenneth/movies/services/MovieService.java
@@ -1,4 +1,19 @@
 package com.kenneth.movies.services;
 
+import com.kenneth.movies.models.Movie;
+import com.kenneth.movies.repositories.MovieRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
 public class MovieService {
+
+    @Autowired
+    private MovieRepository movieRepository;
+
+    public List<Movie> allMovies() {
+        return movieRepository.findAll();
+    }
 }
