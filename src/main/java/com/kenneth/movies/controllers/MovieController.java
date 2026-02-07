eb2b3aaea94ef2d4c7660215a50ea98d49f3ca4b
klavoie1 <kenlavoie19@gmail.com>
2026-02-07 08:01:12 -0500

    added search byImdbId GET mapping

--- src/main/java/com/kenneth/movies/controllers/MovieController.java
+++ src/main/java/com/kenneth/movies/controllers/MovieController.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -21,5 +22,10 @@ public class MovieController {
     @GetMapping
     public ResponseEntity<List<Movie>> getAllMovies() {
         return new ResponseEntity<>(movieService.allMovies(), HttpStatus.OK);
+    }
+
+    @GetMapping("/{imdbId}")
+    public ResponseEntity<Movie> getMovie(@PathVariable String imdbId) {
+        return new ResponseEntity<>(movieService.findMovieByImdbId(imdbId), HttpStatus.OK);
     }
 }
